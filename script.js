5199b3f21dc6120d1130cd8001fb8427649c9540
Nat but on PC <zenzliaa@gmail.com>
2019-07-09 00:47:00 +1000

    Added scrolling feature because I'm cheap and not buying it

--- script.js
+++ script.js
@@ -4,22 +4,28 @@ $(document).ready(function() {
         document.body.scrollTop = 0; // For Safari
         document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     });
-    var PAGES = ["about","projects","gallery","commissions","contact"];
-    var pagePointer = -1;
-    var currentScroll = document.documentElement.scrollTop;
-    var URL = location.href;
-    window.onscroll = function() {
-        var tempPtr;
-        if(document.documentElement.scrollTop > currentScroll && pagePointer > -1.5) {
-            pagePointer += 1;
-            // tempPtr = Math.floor(pagePointer);
-        } else {
-            pagePointer -= 1;
-            // tempPtr = Math.round(pagePointer);
-        }
-        // var elmnt = document.getElementById((PAGES[tempPtr]));
-        console.log("Page Pointer: " + pagePointer);
-        // elmnt.scrollIntoView();+ " Rounded: " + tempPtr);
-    };
+    var PAGES = ["navbar","about","projects","gallery","commissions","contact"];
+   
+    var pagePointer = 0;
+    var URL = window.location.href.split("/")[0] + "//" + window.location.href.split("/")[2];
+    var windowHeight = window.innerHeight;
+    // var PAGESIZE = [0,$("about").position().top,windowHeight+(.1*windowHeight)];
 
+    //[0,windowHeight+70,windowHeight*2+70,windowHeight*3+165,windowHeight*4+258,windowHeight*5+350];
+
+    $(document).on('wheel', function(e) {
+        
+        var delta = e.originalEvent.deltaY;
+    
+        if (delta > 0 && pagePointer+1 < PAGES.length) {
+            pagePointer += 1;
+        } else if (delta < 0 && pagePointer > 0) {
+            pagePointer -= 1;
+        }
+        window.scroll({
+            top:  $("#" + PAGES[pagePointer]).offset().top,
+            behavior: 'smooth'
+        });
+        // $("#" + PAGES[pagePointer])[0].scrollIntoView();
+    });
 });
