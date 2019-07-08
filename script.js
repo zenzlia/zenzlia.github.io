a7792d01b95fef73f7f18e9a65db08e1155899fa
Nat but on PC <zenzliaa@gmail.com>
2019-07-08 22:13:05 +1000

    Page Scroller started

--- script.js
+++ script.js
@@ -4,8 +4,22 @@ $(document).ready(function() {
         document.body.scrollTop = 0; // For Safari
         document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     });
+    var PAGES = ["about","projects","gallery","commissions","contact"];
+    var pagePointer = -1;
+    var currentScroll = document.documentElement.scrollTop;
+    var URL = location.href;
     window.onscroll = function() {
-    
+        var tempPtr;
+        if(document.documentElement.scrollTop > currentScroll && pagePointer > -1.5) {
+            pagePointer += 1;
+            // tempPtr = Math.floor(pagePointer);
+        } else {
+            pagePointer -= 1;
+            // tempPtr = Math.round(pagePointer);
+        }
+        // var elmnt = document.getElementById((PAGES[tempPtr]));
+        console.log("Page Pointer: " + pagePointer);
+        // elmnt.scrollIntoView();+ " Rounded: " + tempPtr);
     };
 
 });
