5996c6f12414bbecf7f8682f3b21ce6278e15b4c
Nat but on PC <zenzliaa@gmail.com>
2019-07-08 20:11:04 +1000

    Added colours

--- script.js
+++ script.js
@@ -4,4 +4,8 @@ $(document).ready(function() {
         document.body.scrollTop = 0; // For Safari
         document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     });
+    window.onscroll = function() {
+    
+    };
+
 });
