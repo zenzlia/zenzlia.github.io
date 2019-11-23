a9ec01b8e1c7c531757cdc07bf10530799e9b804
Zenzlia <natalie.hong@uqconnect.edu.au>
2019-11-23 18:17:59 +1000

    UWU

--- script.js
+++ script.js
@@ -9,8 +9,9 @@ $(document).ready(function() {
         document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     });
     var PAGES = ["navbar","about","projects","gallery","commissions","contact"];
-    $("a").click(function(event) {
+    /*$("a").click(function(event) {
         localStorage.setItem("k", $(event.target)[0].innerText);
         $('#' + data[localStorage.getItem("k")]).css('visibility', 'visible');
-    });
+        
+    });*/
 });
