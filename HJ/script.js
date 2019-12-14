996a92aae78274fa534348224f0a254882d9e462
Zenzlia <natalie.hong@uqconnect.edu.au>
2019-12-14 13:58:05 +1000

    lmao HJ

---
+++ HJ/script.js
@@ -0,0 +1,33 @@
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#blah')
+                .attr('src', e.target.result)
+                .css({"width":"130%", "height":"100%", "margin-bottom":"1000px"});
+            $('#small').css({"visibility":"visible"})
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+$(document).ready(function(){
+    var countdown = 30 * 60 * 1000;
+    var timerId = setInterval(function(){
+    countdown -= 1000;
+    var min = Math.floor(countdown / (60 * 1000));
+    //var sec = Math.floor(countdown - (min * 60 * 1000));  // wrong
+    var sec = Math.floor((countdown - (min * 60 * 1000)) / 1000);  //correct
+
+    if (countdown <= 0) {
+        alert("30 min!");
+        clearInterval(timerId);
+        //doSomething();
+    } else {
+        $("#countdown").html(min + ":" + sec);
+    }
+
+    }, 1000); //1000ms. = 1sec.
+});
