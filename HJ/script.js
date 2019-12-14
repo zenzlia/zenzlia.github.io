3b7ee5881ce81191218029714dfa3a5ac0d93aef
Zenzlia <natalie.hong@uqconnect.edu.au>
2019-12-14 19:35:32 +1000

    work

--- HJ/script.js
+++ HJ/script.js
@@ -14,7 +14,7 @@ function readURL(input) {
 }
 
 $(document).ready(function(){
-    document.body.requestFullscreen();
+    // document.body.requestFullscreen();
     var countdown = 30 * 60 * 1000;
     var timerId = setInterval(function(){
     countdown -= 1000;
